1441ce5298a8dae5816543d4acfc19a387ea8cc0
Yoav Damari <yoav.damari@runi.ac.il>
2024-01-03 17:20:19 +0200

    ad

--- LoanCalc.java
+++ LoanCalc.java
@@ -42,7 +42,7 @@ public class LoanCalc {
 	 */
 	// Side effect: modifies the class variable iterationCounter.
 	public static double bruteForceSolver(double loan, double rate, int n, double epsilon) {
-		double g = 1;
+		double g = n;
 		iterationCounter = 0;
 		while (endBalance(loan, rate, n, g) > 0) {
 			g += epsilon;
